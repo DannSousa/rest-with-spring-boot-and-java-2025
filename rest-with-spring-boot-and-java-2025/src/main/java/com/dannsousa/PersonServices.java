0e2835ad196f96fbfed184801758add789835a01
Dsmatos <daniel._sousa@outlook.com>
2025-12-10 18:13:23 -0300

    Database creation, dependencies and application.yml update

--- rest-with-spring-boot-and-java-2025/src/main/java/com/dannsousa/PersonServices.java
+++ rest-with-spring-boot-and-java-2025/src/main/java/com/dannsousa/PersonServices.java
@@ -3,6 +3,8 @@ package com.dannsousa;
 import com.dannsousa.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,18 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public List<Person> findAll(){
+        logger.info("Finding all people");
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+
+
     public Person findById(String id){
         logger.info("Finding one person");
 
@@ -20,6 +34,32 @@ public class PersonServices {
         person.setFirstName("Daniel");
         person.setLastName("Sousa");
         person.setAddress("Taboão da Serra - São Paulo");
+        person.setGender("Male");
+        return person;
+    }
+
+    public Person create(Person person){
+        logger.info("Creating one person");
+
+        return person;
+    }
+
+    public Person update(Person person){
+        logger.info("Updating one person");
+
+        return person;
+    }
+    public void  delete(String id){
+        logger.info("Deleting one person");
+
+        }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Firstname " + i);
+        person.setLastName("Lastname " + i);
+        person.setAddress("Address " + i);
         person.setGender("Male");
         return person;
     }
